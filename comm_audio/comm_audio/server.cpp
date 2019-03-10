76ea416bf087850410f2346ed5e62178832d63b1
jmkim2392 <jmkim2392@gmail.com>
2019-03-10 01:03:08 -0800

    testing broadcast

--- comm_audio/comm_audio/server.cpp
+++ comm_audio/comm_audio/server.cpp
@@ -32,10 +32,13 @@ SOCKET RequestSocket;
 HANDLE AcceptThread;
 HANDLE RequestReceiverThread;
 HANDLE RequestHandlerThread;
+HANDLE BroadCastThread;
 
 REQUEST_HANDLER_INFO req_handler_info;
+BROADCAST_INFO broadcast_info;
 
 bool isAcceptingConnections = FALSE;
+BOOL isBroadcasting = FALSE;
 WSAEVENT AcceptEvent;
 WSAEVENT RequestReceivedEvent;
 
@@ -78,6 +81,7 @@ void initialize_server(LPCWSTR tcp_port, LPCWSTR udp_port)
 
 	start_request_receiver();
 	start_request_handler();
+	start_broadcast(&udp_socket, udp_port);
 
 	if ((AcceptThread = CreateThread(NULL, 0, connection_monitor, (LPVOID)&tcp_accept_socket, 0, &ThreadId)) == NULL)
 	{
@@ -186,6 +190,29 @@ void start_request_handler()
 	add_new_thread(ThreadId);
 }
 
+void start_broadcast(SOCKET* socket, LPCWSTR udp_port)
+{
+	DWORD ThreadId;
+	size_t i;
+	int portNum;
+	char* port_num = (char *)malloc(MAX_INPUT_LENGTH);
+
+	isBroadcasting = TRUE;
+
+	wcstombs_s(&i, port_num, MAX_INPUT_LENGTH, udp_port, MAX_INPUT_LENGTH);
+	portNum = atoi(port_num);
+
+	broadcast_info.portNum = portNum;
+	broadcast_info.udpSocket = *socket;
+
+	if ((BroadCastThread = CreateThread(NULL, 0, broadcast_audio, (LPVOID)&broadcast_info, 0, &ThreadId)) == NULL)
+	{
+		printf("CreateThread failed with error %d\n", GetLastError());
+		return;
+	}
+	add_new_thread(ThreadId);
+}
+
 /*-------------------------------------------------------------------------------------
 --	FUNCTION:	connection_monitor
 --
@@ -225,6 +252,63 @@ DWORD WINAPI connection_monitor(LPVOID tcp_socket) {
 		{
 			printf("WSASetEvent failed with error %d\n", WSAGetLastError());
 			return WSAGetLastError();
+		}
+	}
+	return 0;
+}
+
+DWORD WINAPI broadcast_audio(LPVOID broadcastInfo)
+{
+	int retVal;
+	BOOL bOpt;
+	SOCKADDR_IN   to;
+	LPBROADCAST_INFO broadcast_info;
+	WSABUF DataBuf;
+	WSAOVERLAPPED Overlapped;
+	DWORD BytesSent = 0;
+	DWORD Flags = 0;
+
+	std::string tempMsg = "Testing Broadcast";
+
+	broadcast_info = (LPBROADCAST_INFO)broadcastInfo;
+
+	bOpt = TRUE;
+	isBroadcasting = TRUE;
+
+	retVal = setsockopt(broadcast_info->udpSocket, SOL_SOCKET, SO_BROADCAST, (char *)&bOpt, sizeof(bOpt));
+	//TODO: temp for testing to be removed
+	//retVal = setsockopt(broadcast_info->udpSocket, SOL_SOCKET, SO_REUSEADDR, (char *)&bOpt, sizeof(bOpt));
+	if (retVal == SOCKET_ERROR)
+	{
+		printf("setsockopt(SO_BROADCAST) failed: %d\n",
+			WSAGetLastError());
+		return -1;
+	}
+	to.sin_family = AF_INET;
+	to.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+	to.sin_port = htons(broadcast_info->portNum);
+
+	// Make sure the Overlapped struct is zeroed out
+	SecureZeroMemory((PVOID)&Overlapped, sizeof(WSAOVERLAPPED));
+
+	// Create an event handle and setup the overlapped structure.
+	Overlapped.hEvent = WSACreateEvent();
+	if (Overlapped.hEvent == WSA_INVALID_EVENT) {
+		printf("WSACreateEvent failed with error: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	// while flag on and no error
+	while (isBroadcasting) 
+	{
+		DataBuf.len = 1024;
+		DataBuf.buf = &tempMsg[0u];
+		retVal = WSASendTo(broadcast_info->udpSocket, &DataBuf, 1, &BytesSent, Flags,(SOCKADDR *)&to, sizeof(to), &Overlapped, NULL);
+		if (retVal == SOCKET_ERROR)
+		{
+			printf("sendto() failed: %d\n", WSAGetLastError());
+			break;
 		}
 	}
 	return 0;
